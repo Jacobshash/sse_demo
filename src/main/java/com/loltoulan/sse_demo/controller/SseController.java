4e7f485d9a7fe3ec2e65161e669391dd989f7926
LOL_toulan <lol_toulan@163.com>
2025-01-15 10:51:54 +0800

    feat(controller): 新增 SSE 数据流控制器方法
    - 在 SseController 中添加了新的 SSE 数据流方法 getFluxSseStream
    - 使用 Flux 生成每秒一个递增的数据流，模拟实时数据推送
    - 新增 spring-boot-starter-actuator 和 spring-webflux依赖

--- src/main/java/com/loltoulan/sse_demo/controller/SseController.java
+++ src/main/java/com/loltoulan/sse_demo/controller/SseController.java
@@ -1,12 +1,16 @@
 package com.loltoulan.sse_demo.controller;
 
+import reactor.core.publisher.Flux;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,8 +21,8 @@ public class SseController {
 
     private final ExecutorService nonBlockingService = Executors.newCachedThreadPool();
 
-    @GetMapping("/sse")
-    public SseEmitter getSseStream() {
+    @GetMapping("/sse/sse-emitter")
+    public SseEmitter getSseEmitterSseStream() {
         SseEmitter emitter = new SseEmitter();
 
         nonBlockingService.execute(() -> {
@@ -33,8 +37,14 @@ public class SseController {
                 emitter.completeWithError(ex);
             }
         });
-
         return emitter;
+    }
+
+    @GetMapping(value = "/sse/flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getFluxSseStream() {
+        // 使用Flux生成每秒一个递增的数据流，用于模拟实时数据推送
+        return Flux.interval(Duration.ofSeconds(1))
+                   .map(sequence -> "Data: " + sequence);
     }
 
 }
